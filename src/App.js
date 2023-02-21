6a8491b3a66343fc4cefcc9a09a3178a89df1d2e
Bhupender98 <108581770+Bhupender98@users.noreply.github.com>
2023-02-21 14:30:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Routes, Route } from "react-router-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Todo from "./Todo";
+import Clint_Panel from "./Clint_Panel";
+import Admin_panel from "./Admin_panel";
 const App = () => {
   return (
     <div>
@@ -11,6 +13,8 @@ const App = () => {
         <Route index element={<SignUp />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
+        <Route path="clientpanel" element={<Clint_Panel />} />
+        <Route path="adminpanel" element={<Admin_panel />} />
         <Route path="todo" element={<Todo />} />
       </Routes>
     </div>
